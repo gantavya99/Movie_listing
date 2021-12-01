5d4f09a9e83ce78d558e6f72cfba56e3362e2033
Gantavya Saraswat <gantavya99@gmail.com>
2021-12-01 23:26:16 +0530

    Added movie model data class

---
+++ rotten_potatoes/lib/model/movie.dart
@@ -0,0 +1,44 @@
+class Movie {
+  Movie(
+    this.title,
+    this.year,
+    this.rated,
+    this.released,
+    this.runtime,
+    this.genre,
+    this.director,
+    this.writer,
+    this.actor,
+    this.plot,
+    this.language,
+    this.country,
+    this.awards,
+    this.poster,
+    this.metaScore,
+    this.imdbRating,
+    this.imdbVotes,
+    this.type,
+    this.response,
+    List<String> images,
+  );
+  late String title;
+  late String year;
+  late String rated;
+  late String released;
+  late String runtime;
+  late String genre;
+  late String director;
+  late String writer;
+  late String actor;
+  late String plot;
+  late String language;
+  late String country;
+  late String awards;
+  late String poster;
+  late String metaScore;
+  late String imdbRating;
+  late String imdbVotes;
+  late String type;
+  late String response;
+  late List<String> images;
+}
